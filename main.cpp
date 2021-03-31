3c87fe37bfefffaf4d2913ca84b28e1213d36357
sth <12021032@zju.edu.cn>
2021-03-31 16:15:33 +0800

    Init program.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "stdio.h"
 #include <iostream>
 #include <tuple>
 #include <fstream>
@@ -7,79 +8,99 @@
 #include <cfloat>
 #include <cmath>
 #include <vector>
+#include <assert.h>
 
+#define EPSILON 1e-6
 using namespace std;
 
-// max different
-// sum different
-// average different
-// file name
-// bias?
 void process(float percent);
 
-auto different(string filepath_1, string filepath_2, string output_file_path, int den) {
-    float max_dif = -FLT_MAX;
-    float sum_dif = 0.0f;
-    int pixel_cnt = 0;
-    // ave_dif = sum_dif / pixel_cnt
-    float ave_dif = 0.0f;
-
-    fstream out_file(output_file_path, fstream::app);
-
-    if (!out_file.good()) {
-        std::cerr << "Open file:" << output_file_path << " failed." << endl;
-        exit(1);
-    }
-    ifstream file_1(filepath_1);
-    ifstream file_2(filepath_2);
-    stringstream str_stream_file_1;
-    stringstream str_stream_file_2;
-    str_stream_file_1 << file_1.rdbuf();
-    str_stream_file_2 << file_2.rdbuf();
-    string str_1;
-    string str_2;
-//    str_stream_file_1.getline()
-
-    while (getline(str_stream_file_1, str_1) && getline(str_stream_file_2, str_2)) {
-        float val_1 = 0.0f;
-        float val_2 = 0.0f;
-        stringstream str_stream_1(str_1);
-        stringstream str_stream_2(str_2);
-        while (str_stream_1 >> val_1 && str_stream_2 >> val_2) {
-            max_dif = max(max_dif, fabs(val_1 - val_2));
-            sum_dif += fabs(val_1 - val_2);
-            pixel_cnt++;
-            char ch;
-            str_stream_1 >> ch;
-            str_stream_2 >> ch;
-        }
-    }
-    ave_dif = sum_dif / pixel_cnt;
-    out_file << den << "," << 2048 << "," << max_dif << "," << sum_dif << "," << ave_dif << "," << pixel_cnt << endl;
-    return tie(max_dif, sum_dif, ave_dif, pixel_cnt);
-}
-
 int main() {
-    vector<int> data_list = {1, 64, 128, 256, 384, 512, 576, 640, 704, 768, 832, 896, 960, 992, 1008, 1016, 1024, 1280,
-                             1408, 1536, 1664, 2048};
-    string output_file_path = "/home/sth/CLionProjects/CompareData_SE_Sth/file_out.csv";
+//    /CLionProjects/SolarEnergy_Chier/OutputFiles/RayDensityExperiment/-150_0_0
+
+    string output_file_path = "/home/sth/CLionProjects/CompareData_SE_Sth/output/compare_result_after_smooth_1024_918.csv";
+    string parent_directory = "918_2.57_-877";
     fstream out_file(output_file_path, fstream::out);
-    out_file << "den,t_den,max,sum,ave,pixel" << endl;
-    out_file.close();
+    out_file << "ray_density,gt_ray_density,max,sum,rms,ave,pixel" << endl;
+    vector<float> result;
 
-    for (int i = 0; i < data_list.size(); ++i) {
-        different("/home/sth/CLionProjects/CompareData_SE_Sth/input/" + to_string(data_list[i]) + "_average.csv",
-                  "/home/sth/CLionProjects/CompareData_SE_Sth/input/2048_average.csv",
-                  output_file_path, data_list[i]);
+    fstream input_file(
+            "/home/sth/CLionProjects/SolarEnergy_Chier/OutputFiles/RayDensityExperiment/" + parent_directory +
+            "/2048_0_receiver_after_smooth.csv");
 
-        if (100 * i / data_list.size() % 10 == 0) {
-            process(static_cast<float>(i) / data_list.size());
+    assert(input_file.good());
+
+    stringstream ss;
+    ss << input_file.rdbuf();
+    input_file.close();
+    string str;
+    getline(ss, str);
+    while (getline(ss, str)) {
+        int index = str.find_last_of(',');
+        float val = atof(str.substr(index + 1).c_str());
+        result.push_back(val);
+    }
+
+    float percent = 0.0f;
+    int cnt = 1024;
+    process(percent);
+
+    for (int i = 1; i <= cnt; i++) {
+        string file_path =
+                "/home/sth/CLionProjects/SolarEnergy_Chier/OutputFiles/RayDensityExperiment/" + parent_directory + "/" +
+                to_string(i) + "_0_receiver_after_smooth.csv";
+        fstream file(file_path);
+
+        if (!file.good()) {
+            cerr << "File " << file_path << " opened fail." << endl;
+            continue;
+        }
+
+        stringstream ss;
+        ss << file.rdbuf();
+        file.close();
+
+        int r = 0;
+        string str;
+        getline(ss, str);
+        float max_dif = -FLT_MAX;
+        float sum_dif = 0.0f;
+        float rms_dif = 0.0f;
+        float ave_dif = 0.0f;
+        int pixel = 0;
+
+        while (getline(ss, str)) {
+            int index = str.find_last_of(',');
+            float val = atof(str.substr(index + 1).c_str());
+            float dif = abs(val - result[r++]);
+
+            pixel++;
+
+            rms_dif += (dif * dif);
+            max_dif = max(max_dif, dif);
+            sum_dif += dif;
+        }
+        rms_dif /= pixel;
+        rms_dif = sqrt(rms_dif);
+        ave_dif = sum_dif / pixel;
+
+        out_file << i << ",2048," << max_dif << "," << sum_dif << "," << rms_dif << "," << ave_dif << "," << pixel
+                 << endl;
+
+        if (static_cast<float>(i) / cnt >= percent + 0.1f) {
+            percent = static_cast<float>(i) / cnt;
+            process(percent);
         }
     }
+    out_file.close();
+    process(1.0f);
     return 0;
 }
 
 void process(float percent) {
+    percent = min(1.0f, percent);
+    percent = max(0.0f, percent);
+
     int num = static_cast<int>(percent / 0.1f);
     cout << "[";
     for (int i = 0; i < num; i++) {
@@ -89,5 +110,7 @@ void process(float percent) {
     for (int i = num; i < 10; i++) {
         cout << " ";
     }
+
     cout << static_cast<int>(percent * 100) << "%" << endl;
+
 }
