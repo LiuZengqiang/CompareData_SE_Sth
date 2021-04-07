3da6a7c2659907a3a475bea85dbefcc902ec20dc
sth <12021032@zju.edu.cn>
2021-04-07 21:09:01 +0800

    Add coefficient of determination.

--- main.cpp
+++ main.cpp
@@ -14,43 +14,71 @@
 using namespace std;
 
 void process(float percent);
+// todo:: add coefficient of determination
+
+// R^2 = 1 - (SS_res)/(SS_tot)
+
+float getCoefficientOfDetermination(vector<float> &measure, vector<float> &ground_truth) {
+    if (measure.size() != ground_truth.size()) {
+        cerr << "Error:: measured result size is not equal to ground truth result size." << endl;
+        return -1.0f;
+    }
+    float SS_res = 0.0f;
+    float SS_tot = 0.0f;
+    float average_y = 0.0f;
+    for (int i = 0; i < measure.size(); i++) {
+        SS_res += pow(measure[i] - ground_truth[i], 2);
+        average_y += measure[i];
+    }
+    average_y /= measure.size();
+    for (int i = 0; i < measure.size(); i++) {
+        SS_tot += pow(measure[i] - average_y, 2);
+    }
+
+    return 1.0f - SS_res / SS_tot;
+}
 
 int main() {
-//    /CLionProjects/SolarEnergy_Chier/OutputFiles/RayDensityExperiment/-150_0_0
 
-    string output_file_path = "/home/sth/CLionProjects/CompareData_SE_Sth/output/compare_result_after_smooth_1024_918.csv";
-    string parent_directory = "918_2.57_-877";
+    // only need change "output_file_path" and "parent_directory"
+//
+//        string output_file_path =
+//                "/home/sth/CLionProjects/CompareData_SE_Sth/output/compare_result_after_smooth_1024_" + it + "_200.csv";
+
+    string parent_directory = "/home/sth/CLionProjects/SolarEnergy_Chier/OutputFiles/RayDensityExperiment/p_[100_2.57_0]_g_64_r_320_d_[1_1024]/";
+    string pre = "before_smooth_d_";
+    string suf = "_r_320.csv";
+
+    string output_file_path =
+            "/home/sth/CLionProjects/CompareData_SE_Sth/output/compare_result_before_smooth_experiment_d_[1_1024]_t_320_with_r2.csv";
     fstream out_file(output_file_path, fstream::out);
-    out_file << "ray_density,gt_ray_density,max,sum,rms,ave,pixel" << endl;
-    vector<float> result;
+    out_file << "ray_density,gt_ray_density,max,sum,rms,ave,pixel,r2" << endl;
 
-    fstream input_file(
-            "/home/sth/CLionProjects/SolarEnergy_Chier/OutputFiles/RayDensityExperiment/" + parent_directory +
-            "/2048_0_receiver_after_smooth.csv");
-
-    assert(input_file.good());
-
+    // construct ground truth result
+    vector<float> ground_truth;
+    fstream ground_truth_file(
+            parent_directory +
+            "before_smooth_d_2048_r_320.csv");
+    assert(ground_truth_file.good());
     stringstream ss;
-    ss << input_file.rdbuf();
-    input_file.close();
+    ss << ground_truth_file.rdbuf();
+    ground_truth_file.close();
     string str;
     getline(ss, str);
     while (getline(ss, str)) {
         int index = str.find_last_of(',');
         float val = atof(str.substr(index + 1).c_str());
-        result.push_back(val);
+        ground_truth.push_back(val);
     }
-
-    float percent = 0.0f;
     int cnt = 1024;
+    float percent = 0.0f;
     process(percent);
-
     for (int i = 1; i <= cnt; i++) {
-        string file_path =
-                "/home/sth/CLionProjects/SolarEnergy_Chier/OutputFiles/RayDensityExperiment/" + parent_directory + "/" +
-                to_string(i) + "_0_receiver_after_smooth.csv";
-        fstream file(file_path);
+        vector<float> result;
 
+        string file_path =
+                parent_directory + pre + to_string(i) + suf;
+        fstream file(file_path);
         if (!file.good()) {
             cerr << "File " << file_path << " opened fail." << endl;
             continue;
@@ -68,24 +96,24 @@ int main() {
         float rms_dif = 0.0f;
         float ave_dif = 0.0f;
         int pixel = 0;
-
+        float R2 = 0.0f;
         while (getline(ss, str)) {
             int index = str.find_last_of(',');
             float val = atof(str.substr(index + 1).c_str());
-            float dif = abs(val - result[r++]);
-
+            result.push_back(val);
+            float dif = abs(val - ground_truth[r++]);
             pixel++;
-
             rms_dif += (dif * dif);
             max_dif = max(max_dif, dif);
             sum_dif += dif;
         }
+
         rms_dif /= pixel;
         rms_dif = sqrt(rms_dif);
         ave_dif = sum_dif / pixel;
-
+        R2 = getCoefficientOfDetermination(result, ground_truth);
         out_file << i << ",2048," << max_dif << "," << sum_dif << "," << rms_dif << "," << ave_dif << "," << pixel
-                 << endl;
+                 << "," << R2 << endl;
 
         if (static_cast<float>(i) / cnt >= percent + 0.1f) {
             percent = static_cast<float>(i) / cnt;
@@ -104,7 +132,7 @@ void process(float percent) {
     int num = static_cast<int>(percent / 0.1f);
     cout << "[";
     for (int i = 0; i < num; i++) {
-        cout << "-";
+        cout << "=";
     }
     cout << ">";
     for (int i = num; i < 10; i++) {
