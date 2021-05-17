6998acc74e5d8045ce0cdbb200c6136b174c2e94
Sth <12021032@zju.edu.cn>
2021-05-17 22:30:33 +0800

    Add done .txt file.

--- main.cpp
+++ main.cpp
@@ -15,9 +15,86 @@ using namespace std;
 int experiment_times = 1;
 
 void process(float percent);
-// todo:: add coefficient of determination
 
 // R^2 = 1 - (SS_res)/(SS_tot)
+
+float getCoefficientOfDetermination(vector<float> &measure, vector<float> &ground_truth);
+
+void getSum(string input_file_path = "");
+
+void done(string variable, string input_directories, string output_path);
+
+//void combineResult(vector<string> &input_file_lists, vector<string> &title_lists, string output_file_path);
+
+void split(string str, vector<float> &v, string spacer);
+
+void splitStr(string str, vector<string> &v, string spacer);
+
+void
+combineResults(vector<string> &input_file_lists, vector<string> titles, string target_title, string output_file_path);
+
+int main() {
+    // note: need divide 320 [100,200,500,800,1000]
+
+//    getSum("/home/sth/CLionProjects/SolarEnergy_Chier/OutputFiles/RayDensityExperiment/p_[50_2.57_0]_g_64_t_320_d_[1_1024]/before_smooth_d_1_r_320.csv");
+
+//    return 0;
+
+
+// ----- 1. Calculate the compare result -----
+// 输入文件夹列表，每个文件夹输出一个结果(一个excel表格)
+    vector<int> input_nums;
+    vector<string> input_directories;
+    vector<string> output_paths;
+    for (int i = 50; i <= 100; i += 50) {
+        input_nums.push_back(i);
+        output_paths.push_back("../output/compare_result_radius_200_alt_" + to_string(i) + "_azi_180.csv");
+    }
+
+    for (int i = 0; i < input_nums.size(); i++) {
+        done(to_string(input_nums[i]), "../input/radius_200_alt_50_azi_180/",
+             output_paths[i]);
+    }
+
+
+//    // only need change target_titles, output_path, input_file_lists and titles
+// ----- 2. Combine the compare results -----
+
+    vector<string> target = {"rms", "max", "r2"};
+    // "radius", "alt" or "azi"
+    string title_pre = "alt";
+    string title_suf = "";
+    vector<string> titles;
+    for (int i = 0; i < input_nums.size(); i++) {
+        titles.push_back(title_pre + to_string(input_nums[i]) + title_suf);
+    }
+
+    string combine_output_path_pre = "../output/all_";
+    string combine_output_path_suf = ".csv";
+    for (string t:target) {
+        string combine_output_path = combine_output_path_pre + t + combine_output_path_suf;
+        combineResults(output_paths, titles, t, combine_output_path);
+    }
+    return 0;
+}
+
+void process(float percent) {
+    percent = min(1.0f, percent);
+    percent = max(0.0f, percent);
+
+    int num = static_cast<int>(percent / 0.1f);
+    cout << "[";
+    for (int i = 0; i < num; i++) {
+        cout << "=";
+    }
+    cout << ">";
+    for (int i = num; i < 10; i++) {
+        cout << " ";
+    }
+
+    cout << static_cast<int>(percent * 100) << "%" << endl;
+
+}
 
 float getCoefficientOfDetermination(vector<float> &measure, vector<float> &ground_truth) {
     if (measure.size() != ground_truth.size()) {
@@ -39,8 +116,7 @@ float getCoefficientOfDetermination(vector<float> &measure, vector<float> &groun
     return 1.0f - SS_res / SS_tot;
 }
 
-
-void getSum(string input_file_path = "") {
+void getSum(string input_file_path) {
     fstream input_file(input_file_path, fstream::in);
     if (!input_file.good()) {
         cerr << "file " << input_file_path << " open failed." << endl;
@@ -63,51 +139,39 @@ void getSum(string input_file_path = "") {
     cout << "sum:" << sum << endl;
 }
 
-void done(string x) {
+void done(string variable, string input_directories, string output_path) {
     // change the parent_directory,pre,suf,output_file_path and ground_truth_file
-    string parent_directory =
-            "/home/sth/CLionProjects/SolarEnergy_Chier/OutputFiles/RayDensityExperiment/different_azimuth/azimuth_" +
-            x + "/";
-
 //    /CLionProjects/SolarEnergy_Chier/OutputFiles/RayDensityExperiment/sun_dir_[90_0]_p_[50_0_0]
 //    /CLionProjects/DifferentAtitude/atitude_40/OutputFiles/sun_dir_[90_40]_p_[50_0_0]
-    string pre = "before_smooth_d_";
-    string suf = "_r_128.csv";
+    string input_filename_pre = "before_smooth_d_";
+    string input_filename_suf = "_r_1.csv";
+
     float coefficience = 1.0f;
-//    [100,200,500,800,1000]
-//    if (x == "100" || x == "200" || x == "500" || x == "800" || x == "1000") {
-//        coefficience /= 320.0f;
-//    }
-//    string output_file_path =
-//            "/home/sth/CLionProjects/CompareData_SE_Sth/output/before_smooth_p_[50_2.57_0]_dir_[90_" + x +
-//            "]_d_[1_512]_r_" + to_string(experiment_times) + "_old.csv";
-    string output_file_path =
-            "/home/sth/CLionProjects/CompareData_SE_Sth/output/RayDensity/diff_azimuth/before_smooth_p[200_2.57_0]_azi_" +
-            x + "_r_128.csv";
 
-    cout << "output file:" << output_file_path << endl;
+    cout << "output path:" << output_path << endl;
 
-    fstream out_file(output_file_path, fstream::out);
+    fstream output_file(output_path, fstream::out);
 
-    out_file << "ray_density,gt_ray_density,max,sum,rms,ave,pixel,r2" << endl;
+    output_file << "ray_density,gt_ray_density,max,sum,rms,ave,pixel,r2" << endl;
 
     // construct ground truth result
     vector<float> ground_truth;
+
     fstream ground_truth_file(
-            parent_directory +
-            "before_smooth_d_2048_r_128.csv");
-//    cout << parent_directory + "before_smooth_d_2048_r_320.csv" << endl;
+            input_directories + "before_smooth_d_2048_r_1.csv");
+
     assert(ground_truth_file.good());
+
     stringstream ss;
     ss << ground_truth_file.rdbuf();
     ground_truth_file.close();
     string str;
-    getline(ss, str);
     while (getline(ss, str)) {
-        int index = str.find_last_of(',');
-        float val = atof(str.substr(index + 1).c_str()) * coefficience;
-        ground_truth.push_back(val);
+        vector<float> temp_vec;
+        split(str, temp_vec, ",");
+        ground_truth.insert(ground_truth.end(), temp_vec.begin(), temp_vec.end());
     }
+
     int cnt = 512;
     float percent = 0.0f;
     process(percent);
@@ -116,36 +180,50 @@ void done(string x) {
 
     for (int i = 1; i <= cnt; i++) {
         vector<float> result;
+        result.clear();
 
         string file_path =
-                parent_directory + pre + to_string(i) + suf;
+                input_directories + input_filename_pre + to_string(i) + input_filename_suf;
         fstream file(file_path);
-//        cout << "file:" << file_path << endl;
         if (!file.good()) {
             cerr << "File " << file_path << " opened fail." << endl;
-            out_file << i << ",2048," << max_dif << "," << sum_dif << "," << rms_dif << "," << ave_dif << "," << pixel
-                     << "," << R2 << endl;
+            output_file << i << ",2048," << max_dif << "," << sum_dif << "," << rms_dif << "," << ave_dif << ","
+                        << pixel
+                        << "," << R2 << endl;
             continue;
         }
+
         stringstream ss;
         ss << file.rdbuf();
+
         file.close();
 
         int r = 0;
         string str;
-        getline(ss, str);
         max_dif = -FLT_MAX;
         sum_dif = 0.0f;
         rms_dif = 0.0f;
         ave_dif = 0.0f;
-        pixel = 0;
+
         R2 = 0.0f;
+
         while (getline(ss, str)) {
-            int index = str.find_last_of(',');
-            float val = atof(str.substr(index + 1).c_str()) * coefficience;
-            result.push_back(val);
-            float dif = abs(val - ground_truth[r++]);
-            pixel++;
+            vector<float> temp_vec;
+            split(str, temp_vec, ",");
+            result.insert(result.end(), temp_vec.begin(), temp_vec.end());
+
+//            float dif = abs(val - ground_truth[r++]);
+//            pixel++;
+//            rms_dif += (dif * dif);
+//            max_dif = max(max_dif, dif);
+//            sum_dif += dif;
+        }
+        pixel = result.size();
+        assert(result.size() == ground_truth.size());
+        for (int j = 0; j < result.size(); j++) {
+            float result_val = result[j];
+            float ground_truth_val = ground_truth[j];
+            float dif = abs(result_val - ground_truth_val);
             rms_dif += (dif * dif);
             max_dif = max(max_dif, dif);
             sum_dif += dif;
@@ -155,19 +233,18 @@ void done(string x) {
         rms_dif = sqrt(rms_dif);
         ave_dif = sum_dif / pixel;
         R2 = getCoefficientOfDetermination(result, ground_truth);
-        out_file << i << ",2048," << max_dif << "," << sum_dif << "," << rms_dif << "," << ave_dif << "," << pixel
-                 << "," << R2 << endl;
+        output_file << i << ",2048," << max_dif << "," << sum_dif << "," << rms_dif << "," << ave_dif << "," << pixel
+                    << "," << R2 << endl;
         if (static_cast<float>(i) / cnt >= percent + 0.1f) {
             percent = static_cast<float>(i) / cnt;
             process(percent);
         }
     }
-    out_file.close();
+    output_file.close();
     process(1.0f);
 
 }
 
-//
 void combineResult(vector<string> &input_file_lists, vector<string> &title_lists, string output_file_path) {
     if (input_file_lists.size() != title_lists.size()) {
         cerr << "file list size is different with title lists." << endl;
@@ -211,7 +288,22 @@ void combineResult(vector<string> &input_file_lists, vector<string> &title_lists
     cout << endl;
 }
 
-void split(string str, vector<string> &v, string spacer) {
+void split(string str, vector<float> &v, string spacer) {
+    v.clear();
+    int pos1, pos2;
+    int len = spacer.length();     //记录分隔符的长度
+    pos1 = 0;
+    pos2 = str.find(spacer);
+    while (pos2 != string::npos) {
+        v.push_back(atof(str.substr(pos1, pos2 - pos1).c_str()));
+        pos1 = pos2 + len;
+        pos2 = str.find(spacer, pos1);    // 从str的pos1位置开始搜寻spacer
+    }
+    if (pos1 != str.length()) //分割最后一个部分
+        v.push_back(atof(str.substr(pos1).c_str()));
+}
+
+void splitStr(string str, vector<string> &v, string spacer) {
     v.clear();
     int pos1, pos2;
     int len = spacer.length();     //记录分隔符的长度
@@ -249,7 +341,7 @@ combineResults(vector<string> &input_file_lists, vector<string> titles, string t
         }
         if (i == 0) {
             vector<string> title_split;
-            split(strs[0], title_split, ",");
+            splitStr(strs[0], title_split, ",");
             for (int j = 0; j < title_split.size(); j++) {
                 if (title_split[j] == target_title) {
                     index = j;
@@ -268,9 +360,9 @@ combineResults(vector<string> &input_file_lists, vector<string> titles, string t
         } else {
             vector<float> values(input_ss.size(), 0.0f);
             for (int j = 0; j < input_ss.size(); j++) {
-                vector<string> v;
+                vector<float> v;
                 split(strs[j], v, ",");
-                values[j] = atof(v[index].c_str());
+                values[j] = v[index];
             }
             for (int j = 0; j < values.size() - 1; j++) {
                 output_file << values[j] << ",";
@@ -279,58 +371,4 @@ combineResults(vector<string> &input_file_lists, vector<string> titles, string t
         }
     }
     output_file.close();
-}
-
-int main() {
-    // note: need divide 320 [100,200,500,800,1000]
-
-//    getSum("/home/sth/CLionProjects/SolarEnergy_Chier/OutputFiles/RayDensityExperiment/p_[50_2.57_0]_g_64_t_320_d_[1_1024]/before_smooth_d_1_r_320.csv");
-
-//    return 0;
-//    vector<int> var;
-//    for (int i = -90; i <= 90; i += 10) {
-//        var.push_back(i);
-//    }
-////    var.push_back(256);
-////    var.push_back(512);
-////    var.push_back(1024);
-//    for (auto &i:var) {
-//        done(to_string(i));
-//    }
-
-    // only need change target_titles, output_path, input_file_lists and titles
-    vector<string> input_file_lists;
-    vector<string> titles;
-    // target title = "rms", "max" or "r2"
-    string target_tile = "r2";
-    string output_path = "../output/RayDensity/diff_azimuth/all_" + target_tile + "_azimuth.csv";
-
-    for (int i = -90; i <= 90; i += 10) {
-//        RayDensity/diff_azimuth/before_smooth_p[200_2.57_0]_azi_" +
-//        x + "_r_128.csv";
-        input_file_lists.push_back(
-                "../output/RayDensity/diff_azimuth/before_smooth_p[200_2.57_0]_azi_" + to_string(i) + "_r_128.csv");
-        titles.push_back("azimuth" + to_string(i));
-    }
-
-    combineResults(input_file_lists, titles, target_tile, output_path);
-    return 0;
-}
-
-void process(float percent) {
-    percent = min(1.0f, percent);
-    percent = max(0.0f, percent);
-
-    int num = static_cast<int>(percent / 0.1f);
-    cout << "[";
-    for (int i = 0; i < num; i++) {
-        cout << "=";
-    }
-    cout << ">";
-    for (int i = num; i < 10; i++) {
-        cout << " ";
-    }
-
-    cout << static_cast<int>(percent * 100) << "%" << endl;
-
 }
